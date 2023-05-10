c0fa9b73dfe4d8df80e642cc0132d19433650b87
Arthur Moro <arthurmoro@gmail.com>
2023-05-10 13:11:00 -0300

    tagmanager

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { Menu } from "./Menu";
 import { Calculator } from "./Calculator";
+import TagManager from "react-gtm-module";
 
 function App() {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: "G-9N6XEJ9B9M" });
+  });
   return (
     <>
       <Menu />
